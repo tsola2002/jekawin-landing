6931241399983284184f5e95211d21213bd032df
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-03-15 10:08:41 +0100

    countdown timer completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,39 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent {
   title = 'jekawin-landing';
+  days: number = 7;
+  hours: number = 57;
+  mins: number = 42;
+  secs: number = 4;
+
+  ngOnInit() {
+    // this.countDownTimer();
+  }
+
+  x = setInterval(() => {
+    var futureDate = new Date("May 15, 2022 15:34:24").getTime(); //WAT time by converting it to millisecond 
+    var today = new Date().getTime();  //we get todays date by converting it to millisecond
+    var distance = futureDate - today;
+    this.days = Math.floor(distance / (1000 * 60 * 60 * 24)); // 20days 22hours 59mins
+    this.hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    this.mins = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+    this.secs = Math.floor(distance % (1000 * 60) / (1000));
+    // console.log(this.hours);
+    if (distance < 0) {
+      clearInterval(this.x);
+    }
+  }, 1000)
+
+  countDownTimer() {
+    var futureDate = new Date("Apr 15, 2022 15:34:24").getTime(); //WAT time by converting it to millisecond 
+    var today = new Date().getTime();  //we get todays date by converting it to millisecond
+    var distance = futureDate - today;
+    this.days = Math.floor(distance / (1000 * 60 * 60 * 24)); // 20days 22hours 59mins
+    this.hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    this.mins = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+    this.secs = Math.floor(distance % (1000 * 60) / (1000));
+    console.log(this.hours);
+  }
 
   subscribeForm = new FormGroup({
     email: new FormControl('', [Validators.required])
